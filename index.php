39af9903120feb66042d7e77ea440dd917ad67d5
Khang Nguyen <khangnhd.it@gmail.com>
2024-05-13 13:01:51 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,5 +3,14 @@
 	//require('closure.php');
 	//require('trait.php');
    //require('iterator.php');
-	require('multiple_function_argument.php');
+//	require('multiple_function_argument.php');
+//	$binaryString = "Hello, world!";
+//	$hexString = bin2hex($binaryString);
+//	
+//	echo $hexString; // Output: 48656c6c6f2c20776f726c6421
+
+	$hexString = "48656c6c6f2c20776f726c6421";
+	$binaryString = hex2bin($hexString);
+	
+	echo $binaryString; // Output: Hello, world!
 ?>
